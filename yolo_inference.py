7e39c92b96e6450def90fb47979ee07f4bc8f8ef
ramindu-Nimex <ramindunimesh61@gmail.com>
2025-01-12 12:57:56 +0530

    Train YOLO on tennis balls

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO 
 
-model = YOLO('yolov8x')
+model = YOLO('models/yolo5_last.pt')
 
-result = model.predict('input_videos/input_video.mp4', save=True)
+result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
 print(result)
 print("boxes:")
 for box in result[0].boxes:
