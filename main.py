98f5a77487395bcea4696962454040cbe1f03258
ramindu-Nimex <ramindunimesh61@gmail.com>
2025-01-13 11:41:21 +0530

    Detect the players

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from utils import (read_video, save_video)
+from trackers import PlayerTracker
 
 def main():
+    # Read Video
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
 
-    save_video(video_frames, "output_videos/output_video.avi")
+    # Detect Players
+    player_tracker = PlayerTracker(model_path='yolov8x')
+
+    player_detections = player_tracker.detect_frames(video_frames)
+
+    # Draw output
+    ## Draw Player Bounding Boxes
+    output_video_frames= player_tracker.draw_bboxes(video_frames, player_detections)
+                                                     
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 if __name__ == "__main__":
     main()
